a652b7ae5ea5fc8d421af7ef04dc6768a9f1d4d6
jretondo <jretondo90@gmail.com>
2023-12-13 00:58:58 -0300

    fix

--- src/utils/facturacion/middleFactu.ts
+++ src/utils/facturacion/middleFactu.ts
@@ -65,7 +65,6 @@ const factuMiddel = () => {
             } else {
                 body.t_fact = 0
                 letra = "X"
-                body.forma_pago = 0
             }
 
             if (body.t_fact === 6 && productsList.totalFact < 10000 && body.cliente_tdoc === 99) {
