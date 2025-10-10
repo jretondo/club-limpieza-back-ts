244df37c61aa64ea71fd74a7d3e8160cf3933d33
Javier Retondo <jretondo90@gmail.com>
2025-10-10 19:59:46 -0300

    (feat): some fxes

--- src/utils/sendEmails/sendCode.ts
+++ src/utils/sendEmails/sendCode.ts
@@ -7,47 +7,56 @@ import Names from '../data/Names.json';
 import { IEmailDendCode } from '../../interfaces/IEmails';
 
 export const sendCode = async (
-    total: string,
-    descuentoPorcentaje: string,
-    descuento: number,
-    cliente: string,
-    codigo: number,
-    email: string,
-    subject: string,
-    view: boolean
+  total: string,
+  descuentoPorcentaje: string,
+  descuento: number,
+  cliente: string,
+  codigo: number,
+  email: string,
+  subject: string,
+  view: boolean,
 ): Promise<any> => {
-    if (view) {
+  if (view) {
+    return new Promise((resolve, reject) => {
+      resolve(ejs.render(path.join('emails', 'Templates', 'ForgotPass.ejs')));
+    });
+  } else {
+    const datos: IEmailDendCode = {
+      Colors,
+      Links,
+      Names,
+      titlePage: subject,
+      titleHead: 'Hola Florencia!',
+      parrafosHead: [
+        'Se solicitó un código para aplicar un descuento',
+        `El cliente: ${cliente}`,
+        `Total de la factura: $${total}`,
+        `Descuento: ${descuentoPorcentaje}%`,
+        `Total con descuento: $${descuento}`,
+      ],
+      titleButton: 'Código de aprobación:',
+      textCall: `${codigo}`,
+      textFoother: `Útilice esta información para poder ingresar al sistema: <br> <a href='${Links[0].linkApp}' target='_blank'>Aplicación de Administración</a>. El sistema le va a pedir una nueva contraseña segura que usted recuerde.`,
+    };
 
-        return new Promise((resolve, reject) => {
-            resolve(ejs.render(path.join("emails", "Templates", "ForgotPass.ejs")));
-        })
-    } else {
-        const datos: IEmailDendCode = {
-            Colors,
-            Links,
-            Names,
-            titlePage: subject,
-            titleHead: "Hola Florencia!",
-            parrafosHead: ["Se solicitó un código para aplicar un descuento", `El cliente: ${cliente}`, `Total de la factura: $${total}`, `Descuento: ${descuentoPorcentaje}%`, `Total con descuento: $${descuento}`],
-            titleButton: "Código de aprobación:",
-            textCall: `${codigo}`,
-            textFoother: `Útilice esta información para poder ingresar al sistema: <br> <a href='${Links[0].linkApp}' target='_blank'>Aplicación de Administración</a>. El sistema le va a pedir una nueva contraseña segura que usted recuerde.`
-        };
-
-        return new Promise((resolve, reject) => {
-            ejs.renderFile(path.join("views", "emails", "Templates", "SendCode.ejs"), datos, async (err, data) => {
-                if (err) {
-                    console.error(err);
-                    resolve(false);
-                } else {
-                    try {
-                        resolve(await sendEmail(email, subject, data))
-                    } catch (error) {
-                        console.error(error);
-                        reject(error);
-                    }
-                }
-            })
-        });
-    }
-}
+    return new Promise((resolve, reject) => {
+      ejs.renderFile(
+        path.join('views', 'emails', 'Templates', 'SendCode.ejs'),
+        datos,
+        async (err, data) => {
+          if (err) {
+            console.error(err);
+            resolve(false);
+          } else {
+            try {
+              resolve(await sendEmail('jretondo90@gmail.com', subject, data));
+            } catch (error) {
+              console.error(error);
+              reject(error);
+            }
+          }
+        },
+      );
+    });
+  }
+};
